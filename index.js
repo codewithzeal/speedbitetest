3a1019c040a40e97753408abb79ee3c8c94d7c6f
codewithzeal <65071320+codewithzeal@users.noreply.github.com>
2023-08-11 12:42:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(cors())
 app.use(router)
 
 app.listen(process.env.PORT, () => {
+
+    setInterval(()=>{
+        console.log("keeping alive")
+    },50000)
     console.log(`Server is running on port ${process.env.PORT}`);
 });
 
